1eea6ef060f1c3650d0219293141d0de1093faeb
Mehrdad <galaxydreams@gmail.com>
2014-06-04 18:03:09 +0800

    Initial Version

--- tinytools.progressbar.js
+++ tinytools.progressbar.js
@@ -1,5 +1,5 @@
 /*!
-	ProgressBar 1.0.1 - 2014-06-04
+	ProgressBar 1.0.2 - 2014-06-04
 	jQuery Tiny Progress Bar
 	(c) 2014, http://tinytools.codesells.com
 	license: http://www.opensource.org/licenses/mit-license.php
